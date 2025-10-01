b6141fbe1ca9068ce20a64eb5267df4d0d9f8e4f
karandesai-sjsu <karan.desai@sjsu.edu>
2025-10-01 10:09:32 -0700

    main file modified

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,41 @@
-# main.py
+import uvicorn
+import os
 from fastapi import FastAPI
 from pymongo import MongoClient
 from gridfs import GridFS
-from api import agents, rag, users
+from contextlib import asynccontextmanager
+# from api import agents, rag, users
+from api import users
 
-app = FastAPI(title="LangTutor API")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    client = MongoClient("ATLAS_URI")
+    db = client["language_app"]
+    fs = GridFS(db)
 
-client = MongoClient("ATLAS_URI")
-db = client["language_app"]
-fs = GridFS(db)
+    app.state.db = db
+    app.state.fs = fs
 
-app.state.db = db
-app.state.fs = fs
+    try:
+        yield
+    finally:
+        client.close()
+
+app = FastAPI(title="LangTutor API" , lifespan=lifespan)
 
 # Register routers
-app.include_router(agents.router, prefix="/agents", tags=["Agents"])
-app.include_router(rag.router, prefix="/rag", tags=["RAG"])
+# app.include_router(agents.router, prefix="/agents", tags=["Agents"])
+# app.include_router(rag.router, prefix="/rag", tags=["RAG"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
 
 @app.get("/")
 def health_check():
     return {"status": "up"}
 
-@app.on_event("shutdown")
-def close_db():
-    client.close()
+def main():
+    HOST = os.getenv("HOST")
+    PORT = int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host=HOST, port=PORT, log_level="info")
+
+if __name__ == "__main__":
+    main()
