02e3c4a7cd2e4b73b4ca401e654f479287dd7c88
Khai Nguyen <khainl1110@gmail.com>
2025-10-12 20:42:12 -0700

    Add context to simple chat bot

--- backend/api/rag.py
+++ backend/api/rag.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
 from models.userschema import SimpleMessageGet, SimpleMessageResponse
-from services.ai_simple_chat import chat_with_llm
+from services.ai_simple_chat import chat_with_llm_and_context
 router = APIRouter()
 
 @router.get("/ai-response", response_model=SimpleMessageResponse)
 def check(payload: SimpleMessageGet):
-    response = chat_with_llm("test_session", payload.input_string)
+    context_str = 'Just know that Asum language is a new language has 2 tones which influence meaning.'
+    response = chat_with_llm_and_context("test_session", payload.input_string, context_str)
     return {"result": response}
     
