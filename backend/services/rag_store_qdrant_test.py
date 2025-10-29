b6d55154101f85f929d5e8a3f32632098c1ba6dc
Khai Nguyen <khainl1110@gmail.com>
2025-10-29 10:30:36 -0700

    Condense test file for qdrant

--- backend/services/rag_store_qdrant_test.py
+++ backend/services/rag_store_qdrant_test.py
@@ -1,10 +1,12 @@
 from rag_store_qdrant import query_qdrant
-from rag_store_test_qdrant import query_test_qdrant
 
-results = query_qdrant("Vietnamese verbs modals", top_k=1)
+coll_name = "vietnamese_store"
+coll_name_test = "vietnamese_test_store"
+results = query_qdrant(coll_name, "Vietnamese verbs modals", top_k=1)
 for res in results:
     print(f"Score: {res.score}, Text: {res.payload.get('text')}")
-print("-----")
-results = query_test_qdrant("Vietnamese pronouns test", top_k=1)
+
+print("\n--- Testing test store ---\n")
+results = query_qdrant(coll_name_test, "Vietnamese pronouns test", top_k=1)
 for res in results:
     print(f"Score: {res.score}, Text: {res.payload.get('text')}")
