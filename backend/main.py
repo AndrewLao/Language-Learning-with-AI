02589a005752505e7548a8ada55ba661386b0ed8
Andrew Lao <43099770+AndrewLao@users.noreply.github.com>
2025-10-31 13:09:07 -0700

    agent function wip memory

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 from gridfs import GridFS
 from contextlib import asynccontextmanager
 # from api import agents, rag, users
-from api import users, rag
+from api import users, agents
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -30,8 +30,9 @@ app = FastAPI(title="LangTutor API" , lifespan=lifespan)
 
 # Register routers
 # app.include_router(agents.router, prefix="/agents", tags=["Agents"])
-app.include_router(rag.router, prefix="/rag", tags=["RAG"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(agents.router, prefix="/agent", tags=["Agent"])
+
 
 @app.get("/")
 def health_check():
